4c0975a68d02526e530f838c480577126a8c02f7
Juippii <A1801097@edu.lapinamk.fi>
2024-10-28 17:08:05 +0200

    User story 1 - Test 2

--- bowling.py
+++ bowling.py
@@ -8,9 +8,13 @@ class BowlingGame:
         self._frames = []
     
     def add_frame(self, frame: Frame) -> None:
+        if len(self._frames) == 10:
+            raise BowlingError
         self._frames.append(frame)
 
     def get_frame_at(self, i: int) -> Frame:
+        if i >= len(self._frames):
+            raise BowlingError
         return self._frames[i]
 
     def calculate_score(self) -> int:
